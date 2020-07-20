3080a7844964f85cb2df0955761bcbde86909d7e
Varshith <svvarsham@gmail.com>
2020-07-20 08:20:44 +0530

    8:20 final

--- Algorithims/workflow.py
+++ Algorithims/workflow.py
@@ -31,9 +31,9 @@ def workflow():
     panel_test = panel_test.drop_duplicates().compute().tolist()
     panel = [x for x in panel_train if x in panel_test]
     train_data = dd.concat([train_x, train_y], axis=1)
-    train_data = train_data.drop(['discount_flag', 'city', 'product_category', 'product_subcategory', 'product'])
-    test_data = test_data.drop(['discount_flag', 'city', 'product_category', 'product_subcategory', 'product'])
-    train_data = train_data.loc[train_x['panel'] == panel]
+    train_data = train_data.drop(['discount_flag', 'city', 'product_category', 'product_subcategory', 'product'], axis=1)
+    test_data = test_data.drop(['discount_flag', 'city', 'product_category', 'product_subcategory', 'product'], axis=1)
+    train_data = train_x['panel'].isin(panel)
     train_data = train_data.groupby(train_data.panel).tolist()
     test_data = test_data.groupby(test_data.panel).tolist()
 
@@ -52,7 +52,7 @@ def workflow():
             x_train, x_test, y_train, y_test = train_test_split(train_x[i], train_y[i], test_size=0.25)
             tpot[i].fit(x_train, y_train)
             score[i] = tpot[i].score(x_test, y_test)
-            test_data[i]['sales'] = tpot[i].predict(test_data[i].drop(['id'], axis=1))
+            test_data[i]['sales'] = tpot[i].predict(test_data[i].drop(['id'], axis=1).compute())
             test_data[i].to_csv(r"Data Files\Submission.csv", mode='a')
             # .values.reshape(len(y_train.index), 1)
         test_pred = dd.read_csv(r"Data Files\Submission.csv")
