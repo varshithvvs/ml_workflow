cf47f5a859c0825128d3faf9aab47279a522759e
Varshith <svvarsham@gmail.com>
2020-07-20 07:49:23 +0530

    7:50 Commit

--- Algorithims/workflow.py
+++ Algorithims/workflow.py
@@ -37,10 +37,8 @@ def workflow():
     train_data = train_data.groupby(train_data.panel).tolist()
     test_data = test_data.groupby(test_data.panel).tolist()
 
-
-    #print(elapsed_init)
     # Create and Train Models
-    if train_x.isna().any() == False:
+    if train_x.isna().any() is False:
         print("\nNo Null Values in model input")
         print("-----------------------------\n\n")
         tpot = TPOTRegressor(generations=5, n_jobs=-1, verbosity=2,
@@ -49,8 +47,8 @@ def workflow():
         for i in range(panel):
             train_data[i] = train_data[i].drop(['panel'], axis=1)
             test_data[i] = test_data[i].drop(['panel'], axis=1)
-            train_y = train_data[i]['sales']
-            train_x = train_data[i].drop(['sales'], axis=1)
+            train_y = train_data[i]['sales'].compute()
+            train_x = train_data[i].drop(['sales'], axis=1).compute()
             x_train, x_test, y_train, y_test = train_test_split(train_x[i], train_y[i], test_size=0.25)
             tpot[i].fit(x_train, y_train)
             score[i] = tpot[i].score(x_test, y_test)
@@ -65,7 +63,6 @@ def workflow():
         return "Null values present"
 
 
-
 if __name__ == '__main__':
     start = datetime.now()
     print(start)
@@ -76,34 +73,3 @@ if __name__ == '__main__':
     print(future.result())
     elapsed = datetime.now() - start
     print("Total elapsed{}".format(elapsed))
-
-'''
-    x_train, x_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.25)
-    classifier = RandomForestClassifier(n_estimators=20, criterion='entropy', n_jobs=-1)
-    classifier.fit(x_train, y_train.values.ravel())
-    y_pred = classifier.predict(x_test)
-    score = classifier.score(x_test, y_test)
-    cm = confusion_matrix(y_test, y_pred)
-
-    features = {}
-    features['train'] = x_train
-    features['target'] = pd.Series(y_train['Survived'], index=y_train.index).astype(int)
-    features['test'] = x_test
-    opt = Optimiser(scoring='accuracy', n_folds=5)
-    space = {
-            'est__strategy': {'space': 'RandomForest'}
-             }
-    score = opt.optimise(None, features)
-    prd = Predictor()
-    prd.fit_predict(score, features)
-    y_pred = pd.read_csv(r'save\Survived_predictions.csv')
-    y_pred = y_pred['Survived_predicted'].round(0)
-    cm = confusion_matrix(y_test, y_pred)
-
-
-    x_train, x_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.25)
-    tpot = TPOTRegressor(generations=20, n_jobs=-1, verbosity=2, use_dask=True)
-    features = x_train.astype(float)
-    target = pd.Series(y_train['Survived'], index=y_train.index).astype(float)
-    tpot.fit(features, target)
-'''
